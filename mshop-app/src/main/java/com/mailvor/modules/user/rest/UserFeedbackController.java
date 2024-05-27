d8b9b4ec780206fb133064df601f248c919eb316
jacky4631 <121659050@qq.com>
2024-05-27 19:18:43 +0800

    实现用户反馈

--- mshop-app/src/main/java/com/mailvor/modules/user/rest/UserFeedbackController.java
+++ mshop-app/src/main/java/com/mailvor/modules/user/rest/UserFeedbackController.java
@@ -11,26 +11,25 @@ import com.mailvor.modules.feedback.domain.MwUserFeedback;
 import com.mailvor.modules.feedback.service.MwUserFeedbackService;
 import com.mailvor.modules.user.param.UserFeedbackParam;
 import io.swagger.annotations.ApiOperation;
-import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
 * @author wangjun
 * @date 2024-05-27
 */
-@AllArgsConstructor
 @RestController
-@RequestMapping("/api/user/feedback")
 public class UserFeedbackController {
 
-    private final MwUserFeedbackService userFeedbackService;
+    @Resource
+    private MwUserFeedbackService userFeedbackService;
 
     @AuthCheck
-    @PostMapping(path = "/add")
+    @PostMapping(path = "/user/feedback/add")
     @ApiOperation("新增用户反馈")
     public ApiResult<Object> create(@Validated @RequestBody UserFeedbackParam param){
         MwUserFeedback userFeedback = new MwUserFeedback();
